2a7fb85dd273afd83dc2fecc88e3f442611af734
Shuk <biz.shuk@gmail.com>
2015-11-11 22:56:35 +0800

    add nvm github

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# [NVM github](https://github.com/creationix/nvm)
+
 node_version="5.0.0"
 
 rm -rf ~/.nvm
@@ -7,7 +9,7 @@ git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git check
 . ~/.nvm/nvm.sh
 
 echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_plugin
-echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.bash_plugin 
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.bash_plugin
 
 
 nvm install v$node_version
