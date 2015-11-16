02210be1386df5ef4fa77f83ec2a05272d1ac551
Shuk <biz.shuk@gmail.com>
2015-11-17 00:40:57 +0800

    update nodejs

--- setup/nodejs.sh
+++ setup/nodejs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # [NVM github](https://github.com/creationix/nvm)
 
-node_version="5.0.0"
+node_version="v5.0.0"
 
 rm -rf ~/.nvm
 git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
@@ -12,6 +12,6 @@ echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_plugin
 echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.bash_plugin
 
 
-nvm install v$node_version
-nvm use v$node_version
+nvm install $node_version
+nvm use $node_version
 nvm alias default $node_version
