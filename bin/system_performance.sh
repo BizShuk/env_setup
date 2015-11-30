70b0797f4aacaf7efe36f7e354d35f300d317bb3
Shuk <biz.shuk@gmail.com>
2015-11-30 15:48:29 +0800

    update

---
+++ bin/system_performance.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+
+
+# system disk 
+echo "--- system disk ---"
+df -h
+
+# cpu
+
+# memory
+
+# network i/o mb
+
+# network request number
+
+
+
+
--- settings.sh
+++ settings.sh
@@ -13,3 +13,14 @@ sdir=$repo_dir
 
 # docker 
 docker_remote_server="10.128.112.15:5000"
+
+
+
+genpasswd(){
+    local l=$1
+    [ "$1" == "" ] && l=16    
+
+    tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${1} | xargs
+
+}
+
