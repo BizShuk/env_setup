3e9f30009a677fb273376c313992702680db99b7
shuk <biz.shuk@gmail.com>
2015-12-21 14:06:48 +0800

    update

--- bin/system_performance.sh
+++ bin/system_performance.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# http://kunhsien.blogspot.tw/2013/11/linuxubuntu.html
+
+sudo fdisk -l #： 列出硬碟分割區
 
 # kernel version
 uname -r
@@ -14,9 +17,19 @@ who -T
 echo "--- system disk ---"
 df -h
 
+# du -sh *
+
+
 # cpu
 
 # memory
+
+# network 
+
+uptime
+
+
+sudo rkhunter --checkall：消滅木馬程式
 
 # network i/o mb
 
@@ -26,3 +39,16 @@ df -h
 
 http://www.tecmint.com/command-line-tools-to-monitor-linux-performance/
 google :linux how to monitor system performance
+
+
+
+self port list
+
+
+**********com*
+http://www.coctec.com/docs/linux/show-post-45715.html
+
+[shuk@shuk]/home/shuk$ vim /var/lo/rkhunter.log
+[shuk@shuk]/home/shuk$ vim /var/log/rkhunter.log
+[shuk@shuk]/home/shuk$ sudo vim /var/log/rkhunter.log
+[shuk@shuk]/home/shuk$ sudo vim /var/log/rkhunter.log
