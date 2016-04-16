4e5b1d4660f66b9499d7f7f181d47a38c013d4cf
shuk <biz.shuk@gmail.com>
2016-04-16 22:32:31 +0800

    update

--- setup/ctags_setup.sh
+++ setup/ctags_setup.sh
@@ -5,7 +5,8 @@ source settings.sh
 ctags_ver="5.8"
 
 pushd $repo_dir/lib/ctag-${ctags_ver}
-    ./configure
+    ./configure --prefix="$lib_dir"
     make
     sudo make install
+    make distclean
 popd
