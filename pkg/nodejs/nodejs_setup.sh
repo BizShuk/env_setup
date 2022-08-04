9822edc1e7cdc1d6c85c91f57697ef2bb6946bbf
shuk <biz.shuk@gmail.com>
2022-08-04 11:12:35 +0800

    Update Node install script

--- pkg/nodejs/nodejs_setup.sh
+++ pkg/nodejs/nodejs_setup.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 # [NVM github](https://github.com/creationix/nvm)
 
-node_version="v6.9.1"  #  v4.4.5 LTS
-
+NODE_VER=${NODE_VER:-v18.7.0}
+NVM_DIR=~/.nvm
 
 
 ### NVM installation """
-rm -rf ~/.nvm
-git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
+[ ! -d $NVM_DIR ] && git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
 
-. ~/.nvm/nvm.sh
-
-echo '# NodeJS for nvm' >> ~/.bash_plugin
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_plugin
-echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"' >> ~/.bash_plugin
+. $NVM_DIR/nvm.sh
 
 ### NODE installation """
-nvm install $node_version
-nvm use $node_version
-nvm alias default $node_version
+nvm install --lts
+nvm install $NODE_VER
+nvm use $NODE_VER
+nvm alias default $NODE_VER
+
+
+### Add NODE to PATH
+cat <<EOF >> ~/.bash_plugin
+# Node
+export NVM_DIR=~/.nvm
+export PATH=\$NVM_DIR/versions/node/$NODE_VER/bin:\$PATH
+EOF
