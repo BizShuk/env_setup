7658ef3fe99cddf204e2ba2e5701d53f42da363b
shuk <biz.shuk@gmail.com>
2022-08-05 13:29:49 +0800

    openssl setup update

--- setup/openssl_mac_setup.sh
+++ setup/openssl_mac_setup.sh
@@ -2,20 +2,22 @@
 
 source settings.sh
 
-OPENSSL_VER="1_1_1q"
-OPEN_SSL_FULLVER="OpenSSL_${OPENSSL_VER}.tar.gz"
-OPENSSL_PATH="$INSTALL_DIR/lib/openssl"
+OPENSSL_VER="1_0_2t"
+OPENSSL_FULLVER="OpenSSL_${OPENSSL_VER}.tar.gz"
+OPENSSL_INSTALL_PATH="${USER_LIB}/${OPENSSL_FULLVER%.tar.gz}"
+OPENSSL_LIB_PATH="${USER_LIB}/openssl"
 
 cd $USER_LIB
 # Warning: It redirect download link in Github, -L option is required. -O option is for remote-name
-curl -LO https://github.com/openssl/openssl/archive/refs/tags/${OPEN_SSL_FULLVER}
-tar -xzvf ${OPEN_SSL_FULLVER}
-cd openssl-${OPEN_SSL_FULLVER%.tar.gz}
+curl -LO https://github.com/openssl/openssl/archive/refs/tags/${OPENSSL_FULLVER}
+tar -xzvf ${OPENSSL_FULLVER}
+cd openssl-${OPENSSL_FULLVER%.tar.gz}
 
-./configure darwin64-x86_64-cc --prefix=${OPENSSL_PATH}
+./configure darwin64-x86_64-cc --prefix=${OPENSSL_INSTALL_PATH}
 make
 make install
+ln -sf ${OPENSSL_INSTALL_PATH} ${OPENSSL_LIB_PATH}
 
-echo "# openssl" >> $INSTALL_DIR/.bash_plugin
-echo "export PATH=$openssl_path/bin:\$PATH" >> $INSTALL_DIR/.bash_plugin
-echo "export MANPATH=$openssl_path/ssl/man:\$MANPATH" >> $INSTALL_DIR/.bash_plugin
+echo "# OpenSSL" >> $INSTALL_DIR/.bash_plugin
+echo "export PATH=$OPENSSL_LIB_PATH/bin:\$PATH" >> $INSTALL_DIR/.bash_plugin
+echo "export MANPATH=$OPENSSL_LIB_PATH/ssl/man:\$MANPATH" >> $INSTALL_DIR/.bash_plugin
