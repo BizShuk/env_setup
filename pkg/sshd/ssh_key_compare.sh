d5466273b0be7ed7e75c3d5dda19088cbb5cf8ae
shuk <biz.shuk@gmail.com>
2025-01-30 17:24:12 +0800

    Update

--- pkg/sshd/ssh_key_compare.sh
+++ pkg/sshd/ssh_key_compare.sh
@@ -2,13 +2,11 @@
 
 . ./settings.sh
 
-private=`ssh-keygen -y -f $INSTALL_DIR/.ssh/id_rsa | cut -d ' ' -f 1,2`
-public=`cat $INSTALL_DIR/.ssh/id_rsa.pub | cut -d ' ' -f 1,2`
+private=$(ssh-keygen -y -f "${INSTALL_DIR}/.ssh/id_rsa" | cut -d ' ' -f 1,2)
+public=$(cat "${INSTALL_DIR}/.ssh/id_rsa.pub" | cut -d ' ' -f 1,2)
 
 if [ "$private" == "$public" ]; then
     echo key id matched
 else
     echo key is not matched
 fi
-
-
