d8f96df765b0658d739877ba24a0b29669dfe8ac
shuk <biz.shuk@gmail.com>
2025-01-30 17:23:04 +0800

    Update

--- pkg/sshd/ssh_keygen.sh
+++ pkg/sshd/ssh_keygen.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-. ./settings.sh
+source ./settings.sh
 
-ssh-keygen -t rsa -b 4096 -C "$email"
-cat $INSTALL_DIR/.ssh/id_rsa.pub >> $INSTALL_DIR/.ssh/authorized_keys
-cat $INSTALL_DIR/.ssh/id_rsa.pub >> $REPO_PKG/sshd/.ssh/id_rsa.pub
-cat $INSTALL_DIR/.ssh/authorized_keys >> $REPO_PKG/sshd/.ssh/authorized_keys
-cat $INSTALL_DIR/.ssh/id_rsa >> $REPO_PKG/sshd/.ssh/id_rsa
-
-
-
+ssh-keygen -t rsa -b 4096 -C "${email}"
+cat "${INSTALL_DIR}/.ssh/id_rsa.pub" >>"${INSTALL_DIR}/.ssh/authorized_keys"
+cat "${INSTALL_DIR}/.ssh/id_rsa.pub" >>"${REPO_PKG}/sshd/.ssh/id_rsa.pub"
+cat "${INSTALL_DIR}/.ssh/authorized_keys" >>"${REPO_PKG}/sshd/.ssh/authorized_keys"
+cat "${INSTALL_DIR}/.ssh/id_rsa" >>"${REPO_PKG}/sshd/.ssh/id_rsa"
 
 echo 'ssh-copy-id -i .ssh/id_rsa.pub <hostname> to copy pubkey to remote'
