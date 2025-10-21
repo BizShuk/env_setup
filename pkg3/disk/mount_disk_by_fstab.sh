8218c913ab141c17354716869c8441a9f49ec838
shuk <biz.shuk@gmail.com>
2025-10-22 04:26:17 +0800

    Update fstab

--- pkg3/disk/mount_disk_by_fstab.sh
+++ pkg3/disk/mount_disk_by_fstab.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # append to /etc/fstab
-UUID=a020e6df-98a0-4d17-bc20-d1b71f29b524 /home/shuk/project ext4 errors=remount-ro 0 1
+UUID=a020e6df-98a0-4d17-bc20-d1b71f29b524 /home/shuk/projects ext4 errors=remount-ro 0 1
 UUID=7d429d65-7f5d-4c45-869c-4b3c4638d970 /home/shuk/project_config ext4 errors=remount-ro 0 1
