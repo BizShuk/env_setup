7e99bc2e7fdfc90293ddb0040587b82677940590
shuk <biz.shuk@gmail.com>
2025-12-10 22:53:40 +0800

    Update

--- pkg/nodejs/npm_setup.sh
+++ pkg/nodejs/npm_setup.sh
@@ -5,8 +5,8 @@
 curl -L http://npmjs.org/install.sh | sh
 
 
-echo "prefix = ${USER_LIB}/npm-global" > "${REPO_DIR}"/bin/bash/.npmrc
-echo 'export PATH=~/lib/.npm-global/bin:$PATH >>~/.bash_plugin'
+echo "prefix = ${USER_LIB}/npm-global" > "${REPO_DIR}/bin/bash/.npmrc"
+echo "export PATH=${USER_LIB}/.npm-global/bin:\$PATH >>~/.bash_plugin"
 
 ln -sf "${REPO_DIR}/bin/bash/.npmrc" "${INSTALL_DIR}/"
 
