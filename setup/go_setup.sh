3f62588ba3450feb8e8b9194d220b4429dae5a1c
shuk <biz.shuk@gmail.com>
2025-12-16 22:00:38 +0800

    Update

--- setup/go_setup.sh
+++ setup/go_setup.sh
@@ -21,11 +21,11 @@ GO_FULLVER="go${GO_VER}.${os}-${GO_ARCH}"
 GO_ROOT="$USER_LIB/$GO_FULLVER"    # go package dir
 GO_PATH="$USER_LIB/go" # Where the go dependency/library downloaded
 
-[ ! -d ${GO_PATH} ] && mkdir -p ${GO_PATH}
+[ ! -d "${GO_PATH}" ] && mkdir -p "${GO_PATH}"
 
 if [ ! -e "$USER_LIB"/"$GO_FULLVER" ]; then
     echo "$GO_FULLVER  installing..."
-    wget https://go.dev/dl/${GO_FULLVER}.tar.gz -O /tmp/"$GO_FULLVER"
+    wget https://go.dev/dl/"${GO_FULLVER}".tar.gz -O /tmp/"$GO_FULLVER"
     # wget https://storage.googleapis.com/golang/"${GO_FULLVER}".tar.gz -O /tmp/"$GO_FULLVER"
 
     tar zxf /tmp/"$GO_FULLVER" -C /tmp
@@ -38,7 +38,7 @@ echo "export GOROOT=${GO_ROOT}" >>"${INSTALL_DIR}"/.bash_plugin
 echo "export GOPATH=${GO_PATH}" >>"${INSTALL_DIR}"/.bash_plugin
 echo "export PATH=\$GOPATH/bin:\$PATH" >>"${INSTALL_DIR}"/.bash_plugin
 echo "# [GOVCS] control which version control tool is used for go get from 1.16" >>"${INSTALL_DIR}"/.bash_plugin
-echo "export GOVCS=git" >>"${INSTALL_DIR}"/.bash_plugin
+# echo "export GOVCS=git" >>"${INSTALL_DIR}"/.bash_plugin
 
 
 [ -L "$USER_BIN"/go ] && echo "go has already installed , now switch to $GO_FULLVER"
