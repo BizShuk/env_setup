91fda893f343ab03e6f5e8280efd378f0d7b5b2c
shuk.liu <biz.shuk@gmail.com>
2026-05-29 21:17:57 +0800

    feat: add system config symlinks to run.sh and update vscode keybindings and settings configurations

--- run.sh
+++ run.sh
@@ -2,3 +2,10 @@
 
 
 ln -sf $HOME/.bash_plugin ./config  
+
+ln -sf /etc/group ./config
+ln -sf /etc/hosts ./config
+ln -sf /etc/localtime ./config
+ln -sf /etc/passwd ./config
+ln -sf /etc/ssl/openssl.cnf ./config
+ln -sf /etc/ssh/ssh_config ./config
