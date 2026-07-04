3e7e80129f48eb57027082b4f8c3b89c85168a71
shuk.liu <biz.shuk@gmail.com>
2026-07-05 02:25:38 +0800

    fix: add compiler attribute definitions for macOS compatibility in general.h

--- pkg/ctags-5.8/general.h
+++ pkg/ctags-5.8/general.h
@@ -56,7 +56,10 @@
 /*  This is a helpful internal feature of later versions (> 2.7) of GCC
  *  to prevent warnings about unused variables.
  */
-#if (__GNUC__ > 2  ||  (__GNUC__ == 2  &&  __GNUC_MINOR__ >= 7)) && !defined (__GNUG__)
+#if defined(__APPLE__)
+# define __unused__
+# define __printf__(s,f)  __attribute__((format (printf, s, f)))
+#elif (__GNUC__ > 2  ||  (__GNUC__ == 2  &&  __GNUC_MINOR__ >= 7)) && !defined (__GNUG__)
 # define __unused__  __attribute__((unused))
 # define __printf__(s,f)  __attribute__((format (printf, s, f)))
 #else
